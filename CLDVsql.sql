0c6266e15d66fec4be82e7994396801095b7b6d7
Khumo Machoga ST10396677 <128786048+MachogaKhumo@users.noreply.github.com>
2025-11-14 15:11:52 +0200

    Add files via upload

---
+++ CLDVsql.sql
@@ -0,0 +1,32 @@
+USE [ABC_RetailDB]; 
+GO
+
+-- Creating Table Users for (Customer or Admin) data
+CREATE TABLE Users (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    Username NVARCHAR(100) NOT NULL UNIQUE, 
+    PasswordHash NVARCHAR(256) NOT NULL,
+    Role NVARCHAR(20) NOT NULL -- 'Customer' or 'Admin'
+);
+
+-- Inserting Predetermined Customer & Admin Login data into Table Users
+INSERT INTO Users (Username, PasswordHash, Role)
+VALUES
+('customer01', 'customerpass123', 'Customer'),
+('admin01', 'adminpass123', 'Admin');
+
+-- Retrieving all records from Table Users
+SELECT *
+FROM Users;
+
+-- Creating Table Cart for shopping cart data
+CREATE TABLE Cart (
+    Id INT PRIMARY KEY IDENTITY (1,1), 
+    CustomerUsername NVARCHAR(100),    
+    ProductId NVARCHAR(100),           
+    Quantity INT
+);
+
+-- Retrieving all records from Table Cart
+SELECT * 
+FROM Cart;
